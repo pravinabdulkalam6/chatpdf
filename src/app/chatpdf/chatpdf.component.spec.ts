7d2d491b9c0549722f514ad40de9ccdfccd58357
Natarajan <natraj2004>
2023-11-06 12:46:22 +0530

    my part

---
+++ src/app/chatpdf/chatpdf.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChatpdfComponent } from './chatpdf.component';
+
+describe('ChatpdfComponent', () => {
+  let component: ChatpdfComponent;
+  let fixture: ComponentFixture<ChatpdfComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ChatpdfComponent]
+    });
+    fixture = TestBed.createComponent(ChatpdfComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/chatpdf/chatpdf.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatpdf',
+  templateUrl: './chatpdf.component.html',
+  styleUrls: ['./chatpdf.component.scss']
+})
+export class ChatpdfComponent {
+
+}
